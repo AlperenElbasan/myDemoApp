f26a7b854541dd7824430f0b5358cc0fccd583b2
AlperenElbasan <aalperenelbasan@gmail.com>
2018-02-16 12:23:59 -0800

    Finished the hw.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,7 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,22 +37,42 @@ public class App
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
           while (sc1.hasNext())
           {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            String value = sc1.next().replaceAll("\\s","");
             inputList.add(value);
           }
-          System.out.println(inputList);
+          String[] arr= inputList.toArray(new String[inputList.size()]);
+          System.out.println("arr: "+Arrays.toString(arr));
 
 
+	      String input11 = req.queryParams("input11");
+          java.util.Scanner sc11 = new java.util.Scanner(input11);
+          sc11.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<String> inputList11 = new java.util.ArrayList<>();
+          while (sc11.hasNext())
+          {
+            String value = sc11.next().replaceAll("\\s","");
+            inputList11.add(value);
+          }
+          String[] arr11 = inputList11.toArray(new String[inputList11.size()]);
+          System.out.println("arr11: "+Arrays.toString(arr11));
+
+
+	      String input12 = req.queryParams("input12").replaceAll("\\s","");
+          int input12AsInt = Integer.parseInt(input12);
+          System.out.println("input12: "+input12AsInt);
+          
           String input2 = req.queryParams("input2").replaceAll("\\s","");
           int input2AsInt = Integer.parseInt(input2);
+	      System.out.println("input2: "+input2AsInt);
 
-          boolean result = App.search(inputList, input2AsInt);
+          String[] result = App.copyPaste(arr,arr11,input12AsInt,input2AsInt);
+		  System.out.println("Result:" +Arrays.toString(result));
 
          Map map = new HashMap();
-          map.put("result", result);
+         map.put("result",Arrays.toString(result));
           return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
